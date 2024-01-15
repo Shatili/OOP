0a79080d82a758b03497cc0fca760927d7a5431c
Shatili <Shatili@yandex.ru>
2024-01-15 16:07:27 +0300

    Polymorphism and magic methods

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -6,6 +6,27 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.average_rating = float()
+    
+    def __str__(self):
+        grades_count = 0
+        courses_in_progress_string = ', '.join(self.courses_in_progress)
+        finished_courses_string = ', '.join(self.finished_courses)
+        for i in self.grades:
+            grades_count += len(self.grades[i])
+        self.average_rating = sum(map(sum, self.grades.values())) / grades_count
+        some_student = f'Имя: {self.name}\n' \
+                       f'Фамилия: {self.surname}\n' \
+                       f'Средняя оценка за домашние задания: {self.average_rating}\n' \
+                       f'Курсы в процессе обучения: {courses_in_progress_string}\n' \
+                       f'Завершенные курсы: {finished_courses_string}'
+        return some_student
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Не студент')
+            return
+        return self.average_rating < other.average_rating                         
+           
     def rate_hw(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
             if course in lecturer.grades:
@@ -23,6 +44,19 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+        self.average_rating = float()
+    def _str_(self):
+        grades_count = 0
+        for i in self.grades:
+            grades_count += len(self.grades[i])
+        self.average_rating = sum(map(sum, self.grades.values())) / grades_count
+        some_student = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_rating}'
+        return some_student 
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Не лектор')
+            return
+        return self.average_rating < other.average_rating
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -33,7 +67,9 @@ class Reviewer(Mentor):
             else:
                 student.grades[course] = [grade]
         else:
-            return 'Ошибка'                     
+            return 'Ошибка'
+    def _str_(self):
+        some_reviewer = f'Имя: {self.name}\nФамилия: {self.surname}'                         
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
